e59242bad59d719c5495d21476ed12630a5c097c
ManuelMedina1 <117885373+ManuelMedina1@users.noreply.github.com>
2024-03-10 00:58:46 -0600

    Update menu_juego.js

--- js/menu_juego.js
+++ js/menu_juego.js
@@ -1,13 +1,27 @@
-
 function main() {
     const btnIngresar = document.getElementById('ingresarBtn');
     btnIngresar.addEventListener('click', () => {
-        // Redigir a la pagina de juego, pero antes
-        // 1. Verificar que el usuario haya ingresado un nombre
-        // 2. Ver si el nombre esta el localStorage, si no esta, agregarlo
-        // 3. Redirigir a la pagina de juego
-        //
-        // Aqui ya se hizo el paso 3
+        //1. Verificar que el usuario haya ingresado un nombre
+        const NombreUsuario = document.getElementById('nombreInput').value;
+        if(!NombreUsuario){
+            mensajeError.textContent = 'Por favor ingresa tu nombre para saber que gran conocedor de la música eres!';
+            return;
+        }else{
+            mensajeError.textContent = '';
+        }
+
+        //2. Guardar el nombre del usuario en el local storage
+        let nombreGuardado = localStorage.getItem('nombre');
+        if (!nombreGuardado) {
+            nombreGuardado = '';
+        }
+        if (nombreGuardado !== NombreUsuario) {
+            nombreGuardado = NombreUsuario;
+            localStorage.setItem('nombre', nombreGuardado);
+            console.log('Se guardó en la consola log');
+        }
+
+        //3. Redirigir al usuario a la página del juego
         window.location.href = irAPaginaHTMLRelativoAURLPrimaria('juego');
     });
 }
